67ed0214fd4fa559364c5454b9864e6f7bf48fa9
Ebenezer Ogungbe <ogungbesegun2000@gmail.com>
2021-08-25 11:52:37 +0100

    changed port to 7000

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.set('view engine','ejs')
-app.set('views',path.join(__dirname , "views")) ;
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-// GET REQUEST 
-app.get('/',(req,res) => {
-    res.send('Hello World') ;
-})
-
+// GET REQUEST
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 // ****** SERVER ********************************
-const port = process.env.PORT || 3000 ;
-app.listen(port , () => {console.log(`Listening on port ${port}......`)});
+const port = process.env.PORT || 7000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}......`);
+});
