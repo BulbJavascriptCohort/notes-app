deb40138e64d26272f2c87d729fcd88c82e68c73
akanbi fatima <akanbifatimah@gmail.com>
2021-08-25 11:21:08 +0100

    Added the views folder

---
+++ data/notes.data.js
@@ -0,0 +1,42 @@
+const notes = [
+    {
+        "title": "",
+        "id": 1,
+        "author-id": ,
+        "image": "",
+        "content": "",
+        "date-published": "",
+    },
+        {
+        "title": "",
+        "id": 2,
+        "author-id": ,
+        "image": "",
+        "content": "",
+        "date-published": "",
+    },
+        {
+        "title": "",
+        "id": 3,
+        "author-id": 
+        "image": "",
+        "content": "",
+        "date-published": "",
+    },
+        {
+        "title": "",
+        "id": 4,
+        "author-id":
+        "image": "",
+        "content": "",
+        "date-published": "",
+    },
+        {
+        "title": "",
+        "id": 5,
+        "author-id": 
+        "image": "",
+        "content": "",
+        "date-published": "",
+    },
+]
