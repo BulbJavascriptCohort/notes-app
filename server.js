aaacfdfd9ff8264546d67e4e6cc618d9f0f06a19
Iyin Oyeleye <iyinoluwa.oyeleye@thebulb.africa>
2021-08-25 11:36:41 +0100

    adds server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname , "views")) ;
+
+// GET REQUEST 
+app.get('/',(req,res) => {
+    res.send('Hello World') ;
+})
+
+
+// ****** SERVER ********************************
+const port = process.env.PORT || 3000 ;
+app.listen(port , () => {console.log(`Listening on port ${port}......`)});
