4cf1034b79c84f6940826a20b4420cbe1eb6ba22
Babatunde Akerele <babsake@gmail.com>
2021-08-25 12:10:28 +0100

    Integrate data with home route

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const data = require('./data/notes.data');
 
-app.set('view engine','ejs')
-app.set('views',path.join(__dirname , "views")) ;
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, "views"));
 
-// GET REQUEST 
-app.get('/',(req,res) => {
-    res.send('Hello World') ;
+// GET REQUEST
+app.get('/', (req, res) => {
+  // res.send('Hello World');
+  res.render('index', {
+    notes: data
+  })
 })
 
 
 // ****** SERVER ********************************
-const port = process.env.PORT || 3000 ;
-app.listen(port , () => {console.log(`Listening on port ${port}......`)});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}......`)
+});
