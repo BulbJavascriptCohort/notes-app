8fa9f2fcc630a1a185ca1b2ae8710fb7682a7c25
Babatunde Akerele <babsake@gmail.com>
2021-09-07 10:01:39 +0100

    retrive only relevant data from localStorage

--- normal/index.js
+++ normal/index.js
@@ -4,82 +4,92 @@ document.getElementById("number_of_notes").textContent = sizeOfNotes
 let noteListElement = document.getElementById("notes")
 
 // populate the localStorage
-for( let i of notes ){
-    localStorage.setItem(i.author_id, JSON.stringify(i))
+for (let i of notes) {
+  localStorage.setItem(`notes-app-${i.id}`, JSON.stringify(i))
 }
+
+console.log('local storage', localStorage)
 
 
 // load the data from the localStorage
-for( let j = 0; j < localStorage.length; j++ ){
-// for(i of notes)
-    let key = localStorage.key(j)
-    let note = JSON.parse( localStorage.getItem(key) ) 
-    
-    let listElement = document.createElement("li")
+for (let j = 0; j < localStorage.length; j++) {
+  let key;
+  // for(i of notes)
+  if (localStorage.key(j).includes('notes-app')) {
+    key = localStorage.key(j)
+  } else {
+    continue;
+  }
 
-    listElement.setAttribute("class", "list-group-item d-flex justify-content-between align-items-start")
-
-    let divElement = document.createElement("div")
-    divElement.setAttribute("class", "ms-2 me-auto")
-
-    let innerDivElement = document.createElement("div")
-    innerDivElement.setAttribute("class", "fw-bold")
-    
-    let innerSpanElement = document.createElement("span")
-
-    let spanElement = document.createElement("span")
-    spanElement.setAttribute("class", "badge bg-primary rounded-pill")
-
-    innerDivElement.textContent = note.title
-    innerSpanElement.textContent = note.content
-
-    spanElement.textContent = note.author_id
-
-    let buttonRow = document.createElement("div")
-    buttonRow.setAttribute("class", "d-grid gap-2")
-    let btnDelete = document.createElement("button")
-    btnDelete.setAttribute("type", "button")
-    btnDelete.setAttribute("class", "btn btn-danger")
-    btnDelete.setAttribute("id", `delete${note.author_id}`)
-    btnDelete.setAttribute("name", note.author_id)
-
-    btnDelete.textContent = "Delete"
-
-    let btnEdit = document.createElement("button")
-    btnEdit.setAttribute("type", "button")
-    btnEdit.setAttribute("class", "btn btn-success")
-    btnEdit.setAttribute("id", `edit${note.author_id}`)
-    btnEdit.textContent = "Edit"
-
-    buttonRow.appendChild(btnEdit)
-    buttonRow.appendChild(btnDelete)
+  let note = JSON.parse(localStorage.getItem(key))
 
 
-    divElement.appendChild(innerDivElement)
-    divElement.appendChild(innerSpanElement)
-   
-    listElement.appendChild(divElement)
-    listElement.appendChild(spanElement)
-    listElement.appendChild(buttonRow)
+  let listElement = document.createElement("li")
+
+  listElement.setAttribute("class", "list-group-item d-flex justify-content-between align-items-start")
+
+  let divElement = document.createElement("div")
+  divElement.setAttribute("class", "ms-2 me-auto")
+
+  let innerDivElement = document.createElement("div")
+  innerDivElement.setAttribute("class", "fw-bold")
+
+  let innerSpanElement = document.createElement("span")
+
+  let spanElement = document.createElement("span")
+  spanElement.setAttribute("class", "badge bg-primary rounded-pill")
+
+  innerDivElement.textContent = note.title
+  innerSpanElement.textContent = note.content
+
+  spanElement.textContent = note.author_id
+
+  let buttonRow = document.createElement("div")
+  buttonRow.setAttribute("class", "d-grid gap-2")
+  let btnDelete = document.createElement("button")
+  btnDelete.setAttribute("type", "button")
+  btnDelete.setAttribute("class", "btn btn-danger")
+  btnDelete.setAttribute("id", `delete${note.author_id}`)
+  btnDelete.setAttribute("name", note.author_id)
+
+  btnDelete.textContent = "Delete"
+
+  let btnEdit = document.createElement("button")
+  btnEdit.setAttribute("type", "button")
+  btnEdit.setAttribute("class", "btn btn-success")
+  btnEdit.setAttribute("id", `edit${note.author_id}`)
+  btnEdit.textContent = "Edit"
+
+  buttonRow.appendChild(btnEdit)
+  buttonRow.appendChild(btnDelete)
 
 
-    noteListElement.appendChild(listElement)
+  divElement.appendChild(innerDivElement)
+  divElement.appendChild(innerSpanElement)
+
+  listElement.appendChild(divElement)
+  listElement.appendChild(spanElement)
+  listElement.appendChild(buttonRow)
+
+
+  noteListElement.appendChild(listElement)
 }
 
 
 document.addEventListener("click", function(amebo) {
-    console.log(amebo.target);
-    const buttonClicked = amebo.target.textContent
-    if (buttonClicked === "Edit"){
-        console.log(amebo.target.getAttribute("id"))
-    }
-    
-    if (buttonClicked === "Delete"){
-        // console.log(amebo.target.getAttribute("id"))
-        let note_id = amebo.target.getAttribute("name")
-        console.log(amebo.target.parentNode.parentNode.remove())
-        localStorage.removeItem(note_id)
-    }   
+  console.log(amebo.target);
+  const buttonClicked = amebo.target.textContent
+  if (buttonClicked === "Edit") {
+    console.log(amebo.target.getAttribute("id"))
+    console.log(amebo);
+  }
+
+  if (buttonClicked === "Delete") {
+    // console.log(amebo.target.getAttribute("id"))
+    let note_id = amebo.target.getAttribute("name")
+    console.log(amebo.target.parentNode.parentNode.remove())
+    localStorage.removeItem(note_id)
+  }
 })
 
 // step
